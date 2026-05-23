75090d59679c80a9de33e1db69aa6898c86c17f9
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-05-23 06:26:15 +0530

    Add files via upload

---
+++ 03_str_functions.py
@@ -0,0 +1,22 @@
+name = "Shiv"
+
+print(len(name))  # Length of the string
+print(name.endswith("v"))  # Check if the string ends with 'v'
+print(name.startswith("Sh"))  # Check if the string starts with 'Sh'
+print(name.find("i"))  # Find the index of 'i' in the string
+print(name.replace("Shiv", "Shiva"))  # Replace 'Shiv' with 'Shiva'
+print(name.upper())  # Convert the string to uppercase
+print(name.lower())  # Convert the string to lowercase
+print(name.isalpha())  # Check if the string contains only alphabetic characters
+print(name.isdigit())  # Check if the string contains only digits
+print(name.isalnum())  # Check if the string contains only alphanumeric characters
+print(name.split("i"))  # Split the string at 'i'
+print(name.strip())  # Remove any leading or trailing whitespace
+print(name[0:3])  # Slicing from index 0 to 3 (excluding 3)
+print(name[-4: -1])  # Negative slicing, from index -4 to the end excluding -1
+print(name[:4])  # Slicing from index 0 to index 4
+print(name[1])  # Accessing the character at index 1
+print(name[2])  # Accessing the character at index 2
+print(name[0:3])  # Slicing from index 0 to 3 (excluding 3)
+print(name[-4: -1])  # Negative slicing, from index -4 to the end excluding -1
+print(name[:4])  # Slicing from index   0 to index 4        
