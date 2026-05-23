82d316a1bc8f1fed220a8bb7e4fe1ebda9414582
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-05-24 01:11:13 +0530

    Add files via upload

---
+++ 04_operatorsy.py
@@ -0,0 +1,61 @@
+# arithmatic operators
+# Arithmatic operators are used to perform basic mathematical operations
+# like addition, subtraction, multiplication, division, etc.
+# They return a numerical value as a result of the operation
+
+
+a = 10 
+
+b = 20
+
+c = a + b 
+
+print(c)
+
+# assignment operators
+# Assignment operators are used to assign values to variables
+# They can also be used to perform operations and assign the result to the variable
+
+
+a = 4-2  #Assign 4-2 in a 
+print(a)
+b =6 
+b += 3 #increment the value of b by 3 anf then assign it to 
+b -= 5 #increment the value of b by 3 anf then assign it to b 
+print(b)
+
+
+
+# comparison operators 
+# it always return boolean values i.e. True or False
+
+c = 5 == 5
+print(c)
+d = 5>4
+print(d)
+
+
+# logical operators
+# Logical operators are used to combine conditional statements
+# and return boolean value i.e. True and False
+
+e = True or  False 
+
+#Truth table of or operator
+print ("True or False is " , True or False)
+print ("True or True is " , True or False)
+print ("False or True is " , False or True)
+print ("False or Flase is " , False or False) 
+
+# Truth table of and operator
+print ("True and False is " , True and False)
+print ("True and True is " , True and False)
+print ("False and True is " , False and True)
+print ("False and Flase is " , False and False)
+
+
+# not operator
+# The not operator is used to reverse the logical state of its operand
+# If the condition is true, it becomes false, and vice versa
+print("not True is ", not True)
+print("not False is ", not False)
