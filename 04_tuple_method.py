fe41a5a29c675f766fa891f34159bb63ccec2ae8
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-06-01 08:08:41 +0530

    Add files via upload

---
+++ 04_tuple_method.py
@@ -0,0 +1,9 @@
+a = (1 ,2, 3, 4, 3, 3, 5)
+print(a)
+
+no = a.count(3)
+print(no)  # Counting occurrences of 3 in the tuple
+print(a.index(4))  # Finding the index of the element 4
+print(a.index(5))
+print( 2 in a )  # Checking if 2 is in the tuple
+print(len(a))  # Getting the length of the tuple
