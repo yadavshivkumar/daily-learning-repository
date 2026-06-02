0e810906f79a9384a845e8ea3c11d96f3bca60bd
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-06-02 15:39:19 +0530

    Add files via upload

---
+++ 01_dict.py
@@ -0,0 +1,9 @@
+marks = {
+    "Harry": 85,
+    "Hermione": 95,
+    "Ron": 75,
+}
+# Display the dictionary and its type
+#list values like key type
+
+print(marks, type(marks))   # dictionary are mutables.
