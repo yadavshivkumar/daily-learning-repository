39ff7dcd39ac10b629e0579083f9aaa9a676cbeb
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-06-03 12:48:36 +0530

    Add files via upload

---
+++ 05_set_union_intersection.py
@@ -0,0 +1,20 @@
+s1 = {1 , 2, 24, 56, 78, 90 , "Shiv" }
+
+s2 = {1 , 2 , 24, 67, 78, 65, "Shiv" }
+
+print(s1.union(s2))  # Union of two sets
+
+print(s1 | s2)  # Another way to get the union of two sets
+
+print(s1.intersection(s2))  # Intersection of two sets
+
+print(s1 & s2)  # Another way to get the intersection of two sets
+
+print(s1.difference(s2))  # Difference of two sets
+
+print(s1 - s2)  # Another way to get the difference of two sets
+
+print(s1.symmetric_difference(s2))  # Symmetric difference of two sets
+
+print(s1 ^ s2)  # Another way to get the symmetric difference of two sets
+
