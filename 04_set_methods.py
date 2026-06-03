1e758581b4d87277f945d3a3ebfafd2377da3bf8
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-06-03 12:48:22 +0530

    Add files via upload

---
+++ 04_set_methods.py
@@ -0,0 +1,9 @@
+s = {1 , 34 , 56, 78, 90 , "Shiv" } 
+
+print(s)
+
+s.add(100)  # Add an integer to the set
+
+print(s)
+print(s.add("New Element"))  # Add a new string element to the set
+print(s)  # Display the updated set
