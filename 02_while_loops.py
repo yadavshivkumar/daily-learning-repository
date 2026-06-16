9228d16ab6ee64918b02caeac73fe3860ad3e2c6
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-06-16 19:27:57 +0530

    Add files via upload

---
+++ 02_while_loops.py
@@ -0,0 +1,15 @@
+i = 1 
+while (i <= 6):
+    print(i)   # while loops operate when a condition is true
+    i += 1    # increment i by 1 each time through the loop
+
+
+
+'''
+Output :
+1
+2
+3
+4
+5
+'''
