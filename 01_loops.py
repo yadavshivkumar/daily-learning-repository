f3a6b10961eccaab400efbd63922daf983b8bc18
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-06-16 19:27:41 +0530

    Add files via upload

---
+++ 01_loops.py
@@ -0,0 +1,16 @@
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+
+# the same task can be done with a loop 
+for i in range(1, 7):
+    print(i)
+
+
+i = 1
+for i in range(1, 7):
+    print("Harry")
+    i += 1
