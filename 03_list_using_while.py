87134a196296f1573763dc4c1a0c81d054ed1553
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-07-03 08:33:43 +0530

    Add files via upload

---
+++ 03_list_using_while.py
@@ -0,0 +1,11 @@
+l = [1 , "Harry" , False , "Subhi" , "Rohan"]
+
+i = 0 
+
+
+while (i< len(l)):
+
+ print(l[i])  # while loops operate when a condition is true
+    i += 1  # increment i by 1 each time through the loop
+
+
