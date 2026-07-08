da45682ea79a8a21b69e11e93bf2628fbe47ed79
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-07-08 07:12:25 +0530

    Add files via upload

---
+++ 04_for_loops.py
@@ -0,0 +1,20 @@
+for i in range (4):
+
+    print(i)  # prints numbers from 0 to 3
+
+for i in range(0, 500, 25):
+    print(i)
+
+
+
+# for loop with a list
+l = [1, 23, 34, 54, 768]
+for i in l:
+    print(i)  # prints each element in the list l
+
+
+
+#for loop with a string
+s = "Harry"
+for i in s:
+    print(i)  # prints each character in the string s
