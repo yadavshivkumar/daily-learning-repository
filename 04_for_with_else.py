99d0f8267ff76913b55c6f32a68fbb69376b5917
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-07-09 07:53:54 +0530

    Add files via upload

---
+++ 04_for_with_else.py
@@ -0,0 +1,7 @@
+l = [1, 34, 24 , 56, 78, 90]
+
+for item in l:
+    print(item)  # prints each element in the list l
+    
+else:
+    print("DONE")  # executes after the loop completes without a break
