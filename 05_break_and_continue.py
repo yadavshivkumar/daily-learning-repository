f0fd0fe6e76078ab1a34673383da134d6af29148
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-07-10 06:13:17 +0530

    Add files via upload

---
+++ 05_break_and_continue.py
@@ -0,0 +1,12 @@
+for i in range(100):
+    if (i==34):
+        break # exits the loop right now
+        print(i) 
+
+
+
+for i in range(100):
+    if (i==34):
+        continue # skips this iteration 
+    print(i)  # prints all numbers except 34 
+
