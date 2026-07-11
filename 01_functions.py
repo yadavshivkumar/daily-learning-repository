bc065af12d086bcd3302dc2c072ae24f4cca5be7
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-07-11 12:04:00 +0530

    Add files via upload

---
+++ 01_functions.py
@@ -0,0 +1,33 @@
+# a = 12
+
+# b = 55
+
+# c = 56
+
+# average = (a+ b+ c)/3
+# print(average)
+
+def avg():
+    a = 12
+    b = 55
+    c = 56
+
+    average = (a + b + c)/3
+    print(average)
+
+avg() #function call 
+avg()
+avg()
+avg()
+avg()
+
+
+def func1():
+    print('hello')  
+
+func1()
+
+def func2():
+    print("Shiv Kumar is a good boy.")
+
+func2()
