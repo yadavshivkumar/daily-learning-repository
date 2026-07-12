3bea1590f3f61666c12e22a6ad2573bae0c15861
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-07-12 06:05:34 +0530

    Add files via upload

---
+++ 01_problem1.py
@@ -0,0 +1,118 @@
+f = open("poem.txt")
+content = f.read()
+if("twinkle" in content ):
+    print("Twinkle is present in content")
+
+else:
+    print("the word twinkle is not present in the content")
+import random
+def game():
+    print("you are playing the game..hibbb 
+          
+          jj 
+    score = random.randint(1, 62)
+
+    with open("hiscore.txt") as f:
+        hiscore = f.read()
+        if(hiscore !=""):
+            hiscore = int(hiscore)
+        else:
+            hiscore = 0 
+
+    print(f"your score: {score}")
+    if(score>hiscore):
+      with open("hiscore.txt", "w") as f:
+            f.write(str(hiscore))
+    return score
+
+game()f.close(import random
+def game():
+    print("you are playing the game..")
+    score = random.randint(1, 62)
+
+    with open("hiscore.txt") as f:
+        hiscore = f.read()
+        if(hiscore !=""):
+            hiscore = int(hiscore)
+        else:
+            hiscore = 0 
+
+    print(f"your score: {score}")
+    if(score>hiscore):
+      with open("hiscore.txt", "w") as f:
+            f.write(str(hiscore))
+    return score
+
+game()import random
+def game():
+    print("you are playing the game..")
+    score = random.randint(1, 62)
+
+    with open("hiscore.txt") as f:
+        hiscore = f.read()
+        if(hiscore !=""):
+            hiscore = int(hiscore)
+        else:
+            hiscore = 0 
+
+    print(f"your score: {score}")
+    if(score>hiscore):
+      with open("hiscore.txt", "w") as f:
+            f.write(str(hiscore))
+    return score
+
+game()import random
+def game():
+    print("you are playing the game..")
+    score = random.randint(1, 62)
+
+    with open("hiscore.txt") as f:
+        hiscore = f.read()
+        if(hiscore !=""):
+            hiscore = int(hiscore)
+        else:
+            hiscore = 0 
+
+    print(f"your score: {score}")
+    if(score>hiscore):
+      with open("hiscore.txt", "w") as f:
+            f.write(str(hiscore))
+    return score
+
+game()import random
+def game():
+    print("you are playing the game..")
+    score = random.randint(1, 62)
+
+    with open("hiscore.txt") as f:
+        hiscore = f.read()
+        if(hiscore !=""):
+            hiscore = int(hiscore)
+        else:
+            hiscore = 0 
+
+    print(f"your score: {score}")
+    if(score>hiscore):
+      with open("hiscore.txt", "w") as f:
+            f.write(str(hiscore))
+    return score
+
+game()import random
+def game():
+    print("you are playing the game..")
+    score = random.randint(1, 62)
+
+    with open("hiscore.txt") as f:
+        hiscore = f.read()
+        if(hiscore !=""):
+            hiscore = int(hiscore)
+        else:
+            hiscore = 0 
+
+    print(f"your score: {score}")
+    if(score>hiscore):
+      with open("hiscore.txt", "w") as f:
+            f.write(str(hiscore))
+    return score
+
+game())
