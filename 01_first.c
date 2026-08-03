eb5842ad81dba814e3ae89db46dc952ab1f62fee
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-08-03 19:44:42 +0530

    Add files via upload

---
+++ 01_first.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main() {
+    printf("Hello World");
+    printf("This is my first C program");
+    return 0;   
+}
+// This is a simple C program that prints "Hello World" to the console
