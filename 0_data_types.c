26c75b5eb801818506a5bdd9cf846be129885f23
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-08-05 18:11:33 +0530

    Add files via upload

---
+++ 0_data_types.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int  main() 
+{
+    int a = 4;
+
+    float  b = 1.5;
+
+    char c = 'R';
+
+    printf("a = %d, b = %.2f, c = %c\n", a, b, c);
+    float sum = a + b + c;
+    
+}
