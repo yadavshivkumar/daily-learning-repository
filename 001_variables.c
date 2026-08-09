ebbe377983bc7b38d802b2a35c5b7c4a572e953c
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-08-09 12:51:03 +0530

    Add files via upload

---
+++ 001_variables.c
@@ -0,0 +1,23 @@
+int a; //interger variable declaration 
+float b; // flaot variable daeclaration 
+char c; // charactaer variable declarartion 
+double d; // double varaiblae declaaration
+
+
+
+// int, float, char, double are data types in C programming language
+// Variable declaration is the process of defining a variable with a specific data type before using it in a program.
+// Variable initialization is the process of assigning an initial value to a variable at the time of its declaration.
+
+
+
+// modulus operator (%) is used to find the remainder of a division operation between two integers.
+// It can only be used with integer data types (int, short, long, etc.)
+// It cannot be used with floating-point data types (float, double, etc.)
+
+//short form of writing a = a + 5 is a += 5
+
+// Increment operator (++) is used to increase the value of a variable by 1.
+// Decrement operator (--) is used to decrease the value of a variable by 1.        
+            
+    
