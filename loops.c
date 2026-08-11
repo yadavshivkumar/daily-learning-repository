ef9a5651057f319afd4e95b2c274ed8f20226752
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-08-11 07:37:27 +0530

    Add files via upload

---
+++ loops.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+int main() {
+ int i = 0 ; 
+ while(i<5) {
+    
+    printf("Happy Birthday !\n");
+    i = i+ 1 ;
+
+ int i = 0 ;
+ while (i<3) {
+    printf("The value of i is %d\n", i);
+    i = 1 + i ;    
+ }   
+}
+return 0;
+}
+ // for loop is used when the number of iterations is known
+ // while loop is used when the number of iterations is not known
+ // while loop is entry controlled loop
+ // do while loop is exit controlled loop
+ // in do while loop the body of the loop is executed at least once
+ // in while loop the body of the loop may not be executed even once
+ // break statement is used to exit the loop
+ // continue statement is used to skip the current iteration of the loop. 
+
+    // infinite loop is a loop that never ends
+    // it can be created using while(1) or for(;;)
+    // it can be exited using break statement or exit(0) function
+    // nested loop is a loop inside a loop
+    // it is used to iterate over a 2D array or matrix
+    // it can be created using for loop or while loop or do while loop
+    // it is important to use proper indentation to avoid confusion
+    // it is also important to use proper variable names to avoid confusion
+    // it is also important to use proper comments to avoid confusion
+    // it is also important to use proper formatting to avoid confusion
+    // it is also important to use proper spacing to avoid confusion
+    // it is also important to use proper syntax to avoid confusion
+    // it is also important to use proper semantics to avoid confusion
+    // it is also important to use proper logic to avoid confusion
+    // it is also important to use proper algorithms to avoid confusion 
+
+    //interpreter is a program that executes the code line by line
+    //compiler is a program that converts the code into machine code
+    //linker is a program that links the object files to create an executable file
+    //loader is a program that loads the executable file into memory and executes it
+    //debugger is a program that helps to find and fix errors in the code
+    //IDE is a program that provides a complete environment for coding, compiling, debugging and executing
+    //syntax error is an error that occurs when the code does not follow the rules of the language
+    //semantic error is an error that occurs when the code does not produce the expected output
+    //logical error is an error that occurs when the code produces the wrong output
+    //runtime error is an error that occurs when the code is executed               
+
+    //increment operator is used to increase the value of a variable by 1
+    //decrement operator is used to decrease the value of a variable by 1
+    //it can be used in two ways: prefix and postfix
+    //prefix: ++i or --i
+    //postfix: i++ or i--
+    //prefix: the value of the variable is changed before it is used in the expression
