2aeb2caac441f230a0399de1f032363a328239b3
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-08-13 09:32:25 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,22 +1,15 @@
-#include<stdio.h> #include<stdlib.h>  #include<time.h>
-int main() {srand(time(0));
-    int player, computer = rand() % 3 + 0;
-    /*   0 --> Snake   1 --> Water    2 --> Gun */
-   printf("Choose 0 for Snake, 1 for Water, 2 for Gun \n");
-   scanf("%d", &player); printf("%d", computer);
-   if (player == 0 && computer == 1) {
-       printf("Player wins! Snake drinks the water\n");
-   } else if (player == 1 && computer == 0) {
-       printf("Computer wins! Snake drinks the water\n");
-   } else if (player == 1 && computer == 2) {
-       printf("Player wins! Water damages the gun\n");
-   } else if (player == 2 && computer == 1) {
-       printf("Computer wins! Water damages the gun\n");
-   } else if (player == 2 && computer == 0) {
-       printf("Player wins! Gun kills the snake\n");
-   } else if (player == 0 && computer == 2) {
-       printf("Computer wins! Gun kills the snake\n");
-   } else if (player == computer) {
-       printf("It's a draw!\n");
-   } else {       printf("Invalid input! Please choose 0, 1, or 2.\n");
-   }    return 0; }
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    // Seed the random number generator
+    srand(time(NULL));
+
+    // Generate random number between 1 and 100
+    int randomNumber = (rand() % 100) + 1;
+
+    printf("Random number between 1 and 100: %d\n", randomNumber);
+   
+    return 0;
+}
