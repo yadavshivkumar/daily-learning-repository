1e58e31bec208d9bcf0f590ee0d7edf7c327d11b
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-08-23 08:00:24 +0530

    Add files via upload

---
+++ 01.intro_to_strings.py
@@ -0,0 +1,11 @@
+name = "Shiv" 
+a = 'harry'       # Single quoted string
+b = "harry"      # Double quoted string
+c = '''harry'''  # Triple  quoted string"
+
+nameshort = name[0:3] # start from index 0 all the way till 3 (excludind 3 )
+character1 = name[1]
+character2 = name[2]
+print(nameshort)
+print(character1)
+print(character2)   
