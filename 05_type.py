21de0db3fc9bc36111db9b287b6f7e12d0ef781b
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-08-31 08:13:30 +0530

    Add files via upload

---
+++ 05_type.py
@@ -0,0 +1,16 @@
+a = 31
+t = type(a) # class <int>
+
+print(t) # Output: <class 'int'>
+
+
+b = "SHIV"
+t = type (b) # class <str>
+print(t) # Output: <class 'str'>
+
+c = 3.14
+t = type(c) # class <float>
+print(t) # Output: <class 'float'>
+
+a = float("31.3")
+print(a)
