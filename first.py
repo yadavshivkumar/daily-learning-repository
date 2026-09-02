c1db0758da6763947a7a23c297e371010058db1f
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-09-02 08:59:03 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,6 @@
+print("Hello World") 
+print("Hello everyone")
+print("This is a Python script.")
+print("hii everyone I am Shiv, I am learning python and I am very exicted" \
+"to learn python. It is very interesting langaguge and I am " \
+"enjoying a lot. I will also learn about Git and Github soon.")
