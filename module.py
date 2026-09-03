6a4c051651ecaa2d7b60d73a36bfeca01f3fd77f
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-09-03 08:32:41 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,14 @@
+import pyjokes
+
+# print("printing jokes...")
+
+# This print a random joke
+# joke = pyjokes. get_joke()
+print (joke)
+
+
+
+# so thanks 
+# that was my program
+# another line
+# yet another line 
