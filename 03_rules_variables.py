c4c00488f784c6b187a74b99b6b07e0fa0d1562c
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-09-15 08:16:43 +0530

    Add files via upload

---
+++ 03_rules_variables.py
@@ -0,0 +1,12 @@
+a = 23
+
+aaa = 42
+
+harry = 12
+
+sameer = 34
+
+_sameer = 56
+    
+# @sameer = 78 # @sameer is not a valid variable name because 
+# it starts with a special character and variable names cannot start with a digit or special character
