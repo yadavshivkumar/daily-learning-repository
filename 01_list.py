ec1769c0af00acb113b3d18360dc0f65aad506ab
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-09-22 18:45:18 +0530

    Add files via upload

---
+++ 01_list.py
@@ -0,0 +1,9 @@
+friends = ["Apple", "Orange","5", "34.65", "False", "Aakash", "Rohan"]
+
+print(friends[0])
+friends[0] = "Grapes" #  strings are immutable but  lists are mutable    
+
+print(friends[0])
+print(len(friends)) # length of the list
+print(friends[1:5]) # slicing the list
+
