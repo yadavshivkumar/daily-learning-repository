8cc10e755d1be972ab4951164ee184537c52e61c
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-09-24 06:51:17 +0530

    Add files via upload

---
+++ 03_tuple.py
@@ -0,0 +1,8 @@
+a = (1 ,2, 3, 4, 5)
+
+a = (1,)   # Tuple with a single element must have a comma
+
+a[0] = 2  # Tuples are immutable, this will raise an error
+
+print(type(a)) # Tuple is immutable, cannot change its elements
+
